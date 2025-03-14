ab023557021a9549847388d9c8833ad371099716
Kimberly Meechan <24316371+K-Meech@users.noreply.github.com>
2025-03-14 17:31:09 +0000

    add minimal script for pyperf error

---
+++ src/minimal_pyperf_error.py
@@ -0,0 +1,21 @@
+import pyperf
+import zarr
+
+# This gives ModuleNotFoundError: No module named 'packaging' on python 3.12, but seems to work with 3.11 and 3.13
+# conda create -n pyperf-env python=3.12
+# conda activate pyperf-env
+# pip install zarr
+# pip install pyperf
+
+
+def main():
+    runner = pyperf.Runner()
+    runner.timeit(
+        name="sort a sorted list",
+        stmt="sorted(s, key=f)",
+        setup="f = lambda x: x; s = list(range(1000))"
+        )
+
+
+if __name__ == "__main__":
+    main()
