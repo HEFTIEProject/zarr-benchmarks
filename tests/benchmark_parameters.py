8508a1e684c7207d318ced991e277e05efa99c30
Kimberly Meechan <24316371+K-Meech@users.noreply.github.com>
2025-03-19 16:00:30 +0000

    reduce combinations

--- tests/benchmark_parameters.py
+++ tests/benchmark_parameters.py
@@ -1,28 +1,22 @@
 import zarr
 
-# CHUNK_SIZE = (400, 300, 200, 100)
-CHUNK_SIZE = (100,)
-COMPRESSORS = (
-    zarr.codecs.GzipCodec(),
-    zarr.codecs.BloscCodec(),
-    zarr.codecs.ZstdCodec(),
-)
+CHUNK_SIZE = (300, 200, 100)
 
-BLOSC_CLEVEL = (1, 5, 9)  # range 1-9
+BLOSC_CLEVEL = (1, 3, 5)  # range 1-9
 BLOSC_SHUFFLE = (
-    zarr.codecs.BloscShuffle.bitshuffle,
-    zarr.codecs.BloscShuffle.shuffle,
+    # zarr.codecs.BloscShuffle.bitshuffle,
+    # zarr.codecs.BloscShuffle.shuffle,
     zarr.codecs.BloscShuffle.noshuffle,
 )
 BLOSC_CNAME = (
     # zarr.codecs.BloscCname.blosclz,
     # zarr.codecs.BloscCname.lz4,
     # zarr.codecs.BloscCname.lz4hc,
-    zarr.codecs.BloscCname.zlib,
+    # zarr.codecs.BloscCname.zlib,
     zarr.codecs.BloscCname.zstd,
 )
 
-GZIP_LEVEL = (1, 5, 9)  # range 1-9
+GZIP_LEVEL = (1, 3, 5)  # range 1-9
 
 # range 1-22 (according to zstd docs, levels>=20 should be used with caution, as they need more memory)
 ZSTD_LEVEL = (1, 7, 13)
