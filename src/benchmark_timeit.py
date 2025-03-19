becd2724bb78008beb7ef710de77a299c1cac086
Kimberly Meechan <24316371+K-Meech@users.noreply.github.com>
2025-03-19 08:53:30 +0000

    add other benchmark function files

---
+++ src/benchmark_timeit.py
@@ -0,0 +1,63 @@
+import pyperf
+from pathlib import Path
+import imageio.v3 as iio
+import numpy as np
+import zarr
+import shutil
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def read_image(image_dir_path: Path) -> np.array:
+    """Read a series of jpeg to one 3D numpy array"""
+    image_slices = []
+    for image_slice in image_dir_path.iterdir():
+        if not image_slice.is_file():
+            continue
+
+        image_slices.append(iio.imread(image_slice))
+
+    return np.stack(image_slices, axis=0)
+
+
+def write_to_zarr(image: np.array, output_path: Path, chunk_size: tuple[int]) -> None:
+    zarr_array = zarr.create_array(
+        store=output_path, shape=image.shape, chunks=chunk_size, dtype=image.dtype
+    )
+    zarr_array[:] = image
+
+
+def benchmark_write_to_zarr(
+    image: np.array, output_path: Path, chunk_size: tuple[int]
+) -> float:
+    logger.info(f"Starting zarr write benchmarking run - chunks {chunk_size}...")
+
+    # Ideally wouldn't include removal of any existing array in total time
+    if output_path.exists():
+        shutil.rmtree(output_path)
+
+    write_to_zarr(image, output_path, chunk_size)
+
+
+def run_profiling() -> None:
+    # runner = pyperf.Runner(values=5, warmups=1, processes=20)
+    runner = pyperf.Runner(values=2, warmups=1, processes=2)
+
+    # image = read_image(Path("data/_200.64um_LADAF-2021-17_heart_complete-organ_pag-0.10_0.03_jp2_"))
+
+    chunk_sizes = [(200, 200, 200), (100, 100, 100)]
+    for chunk_size in chunk_sizes:
+        # Could use setup to also remove any existing zarr array, so not included in the final timing
+        runner.timeit(
+            name=f"write_to_zarr-{chunk_size}",
+            stmt="benchmark_write_to_zarr(image, output_path, chunk_size)",
+            setup=f"chunk_size={chunk_size}; output_path=Path('output/heart-example.zarr'); image = np.ones(shape=(500, 500, 500))",
+            globals=globals(),
+        )
+
+
+if __name__ == "__main__":
+    # To dump results to json, run with: python src/benchmark_timeit.py -o output/bench.json
+    logging.basicConfig(level=logging.INFO)
+    run_profiling()
