3300033aba6c949c0f75f004f31be66bfbb97574
Kimberly Meechan <24316371+K-Meech@users.noreply.github.com>
2025-03-19 14:15:00 +0000

    rough blosc setting parameters

--- tests/benchmark_parameters.py
+++ tests/benchmark_parameters.py
@@ -1,8 +1,23 @@
 import zarr
 
-CHUNK_SIZE = (400, 300, 200, 100)
+# CHUNK_SIZE = (400, 300, 200, 100)
+CHUNK_SIZE = (100,)
 COMPRESSORS = (
     zarr.codecs.GzipCodec(),
     zarr.codecs.BloscCodec(),
     zarr.codecs.ZstdCodec(),
+)
+
+BLOSC_CLEVEL = (1, 3, 5, 7, 9)  # range 1-9
+BLOSC_SHUFFLE = (
+    zarr.codecs.BloscShuffle.bitshuffle,
+    zarr.codecs.BloscShuffle.shuffle,
+    zarr.codecs.BloscShuffle.noshuffle,
+)
+BLOSC_CNAME = (
+    zarr.codecs.BloscCname.blosclz,
+    zarr.codecs.BloscCname.lz4,
+    zarr.codecs.BloscCname.lz4hc,
+    zarr.codecs.BloscCname.zlib,
+    zarr.codecs.BloscCname.zstd,
 )
