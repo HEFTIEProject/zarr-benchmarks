bbc1dd9698f324808c33697696cbe7493f00b365
ruaridhg <ruaridhg@users.noreply.github.com>
2025-03-21 16:49:24 +0000

    Fix linting

--- src/parse_json.py
+++ src/parse_json.py
@@ -7,8 +7,13 @@ with open(path_to_file, "r") as in_file_obj:
     # convert the text into a dictionary
     data = json.loads(text)
 
-machine_info = data['machine_info']['cpu']
-for test in range(len(data['benchmarks'])):
-    print(data['benchmarks'][test]['group'])
-    print(data['benchmarks'][test]['stats'])
-    print(data['benchmarks'][test]['extra_info']['compression_ratio'] if 'compression_ratio' in data['benchmarks'][test]['extra_info'] else None)
+machine_info = data["machine_info"]["cpu"]
+for test in range(len(data["benchmarks"])):
+    print(data["benchmarks"][test]["group"])
+    print(data["benchmarks"][test]["name"])
+    print(data["benchmarks"][test]["stats"])
+    print(
+        data["benchmarks"][test]["extra_info"]["compression_ratio"]
+        if "compression_ratio" in data["benchmarks"][test]["extra_info"]
+        else None
+    )
