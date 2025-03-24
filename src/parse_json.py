1e52ceabd3b3f1ac9d17a79a482dfed1638cde14
ruaridhg <ruaridhg@users.noreply.github.com>
2025-03-24 11:22:59 +0000

    clean up parse_json.py

--- src/parse_json.py
+++ src/parse_json.py
@@ -1,19 +1,23 @@
 import json
 
-path_to_file = "data/json/0007_small_img.json"
 
-with open(path_to_file, "r") as in_file_obj:
-    text = in_file_obj.read()
-    # convert the text into a dictionary
-    data = json.loads(text)
+def load_benchmarks_json(path_to_file: str) -> dict:
+    with open(path_to_file, "r") as in_file_obj:
+        text = in_file_obj.read()
+        # convert the text into a dictionary
+        return json.loads(text)
 
-machine_info = data["machine_info"]["cpu"]
-for benchmark in range(len(data["benchmarks"])):
-    print(data["benchmarks"][test]["group"])
-    print(data["benchmarks"][test]["name"])
-    print(data["benchmarks"][test]["stats"])
-    print(
-        data["benchmarks"][test]["extra_info"]["compression_ratio"]
-        if "compression_ratio" in data["benchmarks"][test]["extra_info"]
-        else None
-    )
+
+if __name__ == "__main__":
+    path_to_file = "data/json/0007_small_img.json"
+    data = load_benchmarks_json(path_to_file)
+    machine_info = data["machine_info"]["cpu"]
+    for benchmark in range(len(data["benchmarks"])):
+        print(data["benchmarks"][benchmark]["group"])
+        print(data["benchmarks"][benchmark]["name"])
+        print(data["benchmarks"][benchmark]["stats"])
+        print(
+            data["benchmarks"][benchmark]["extra_info"]["compression_ratio"]
+            if "compression_ratio" in data["benchmarks"][benchmark]["extra_info"]
+            else None
+        )
