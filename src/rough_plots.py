af4d331970bb0d07e03d167efdccf8281e23d506
Kimberly Meechan <24316371+K-Meech@users.noreply.github.com>
2025-04-03 10:00:32 +0100

    rough dataframe preparation

---
+++ src/rough_plots.py
@@ -0,0 +1,60 @@
+import json
+import pandas as pd
+
+
+def load_benchmarks_json(path_to_file: str) -> dict:
+    with open(path_to_file, "r") as in_file_obj:
+        text = in_file_obj.read()
+        # convert the text into a dictionary
+        return json.loads(text)
+
+
+def prepare_benchmarks_dataframe(json_dict: dict) -> pd.DataFrame:
+    benchmark_df = pd.json_normalize(json_dict["benchmarks"])
+
+    # copy compression ratio from read benchmarks to write benchmarks
+    param_cols = [col for col in benchmark_df if col.startswith("params")]
+    benchmark_df["compression_ratio"] = benchmark_df.groupby(
+        param_cols, dropna=False, as_index=False
+    )["extra_info.compression_ratio"].transform("max")
+
+    # combine compression level columns for different compressors
+    benchmark_df["compression_level"] = benchmark_df[
+        ["params.blosc_clevel", "params.gzip_level", "params.zstd_level"]
+    ].max(axis=1)
+
+    # create column for type of compressor
+    benchmark_df["compressor"] = ""
+    blosc_compressors = (
+        "blosc-"
+        + benchmark_df.loc[
+            ~benchmark_df["params.blosc_clevel"].isna(), "params.blosc_cname"
+        ]
+    )
+    benchmark_df.loc[~benchmark_df["params.blosc_clevel"].isna(), "compressor"] = (
+        blosc_compressors
+    )
+    benchmark_df.loc[~benchmark_df["params.gzip_level"].isna(), "compressor"] = "gzip"
+    benchmark_df.loc[~benchmark_df["params.zstd_level"].isna(), "compressor"] = "zstd"
+
+    # remove un-needed columns
+    stats_cols = [col for col in benchmark_df if col.startswith("stats")]
+    benchmark_df = benchmark_df[
+        [
+            "group",
+            "compressor",
+            "compression_level",
+            "compression_ratio",
+            "params.chunk_size",
+        ]
+        + stats_cols
+    ]
+    benchmark_df = benchmark_df.rename(columns={"params.chunk_size": "chunk_size"})
+
+    return benchmark_df
+
+
+if __name__ == "__main__":
+    json_path = "data/json/0007_zarr-python-v2.json"
+    json_dict = load_benchmarks_json(json_path)
+    df = prepare_benchmarks_dataframe(json_dict)
