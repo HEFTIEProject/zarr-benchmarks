9d2dd907ae952a489591c7c6bc510a2dc84c3499
Kimberly Meechan <24316371+K-Meech@users.noreply.github.com>
2025-06-04 13:18:37 +0100

    add test for read_write_zarr import

--- tests/tests/test_write_zarr.py
+++ tests/tests/test_write_zarr.py
@@ -1,3 +1,6 @@
+from importlib.metadata import version
+from importlib.util import find_spec
+
 import numpy as np
 import pytest
 
@@ -24,3 +27,34 @@ def test_write_empty_chunks(tmp_path, write_empty_chunks):
 
     assert (store_path / ".zarray").exists()
     assert (store_path / "0.0.0").exists() == write_empty_chunks
+
+
+def test_read_write_zarr_import():
+    """Check that the correct package is imported as read_write_zarr for each tox environment"""
+
+    tensorstore_installed = find_spec("tensorstore") is not None
+    zarr_python_installed = find_spec("zarr") is not None
+
+    # one (and only one) of tensorstore / zarr python should be installed in each env
+    assert tensorstore_installed != zarr_python_installed
+
+    if tensorstore_installed:
+        assert (
+            read_write_zarr.__name__
+            == "zarr_benchmarks.read_write_zarr.read_write_tensorstore"
+        )
+        return
+
+    if version("zarr").split(".")[0] == "2":
+        assert (
+            read_write_zarr.__name__
+            == "zarr_benchmarks.read_write_zarr.read_write_zarr_python_v2"
+        )
+        return
+
+    if version("zarr").split(".")[0] == "3":
+        assert (
+            read_write_zarr.__name__
+            == "zarr_benchmarks.read_write_zarr.read_write_zarr_python_v3"
+        )
+        return
