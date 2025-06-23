07b3356191a4e00985edfb4cdc34dd658ff25ee1
ruaridhg <32329546+ruaridhg@users.noreply.github.com>
2025-06-23 11:15:57 +0100

    Update src/zarr_benchmarks/create_plots.py
    Co-authored-by: Kimberly Meechan <24316371+K-Meech@users.noreply.github.com>

--- src/zarr_benchmarks/create_plots.py
+++ src/zarr_benchmarks/create_plots.py
@@ -520,7 +520,7 @@ def create_read_write_plots(benchmarks_df: pd.DataFrame, plots_dir: Path) -> Non
     write_chunks_128_spec2 = read_write_benchmarks[
         (read_write_benchmarks.group == "write")
         & (read_write_benchmarks.chunk_size == 128)
-        & (benchmarks_df.zarr_spec == 2)
+        & (read_write_benchmarks.zarr_spec == 2)
     ]
 
     plot_relplot_benchmarks(
